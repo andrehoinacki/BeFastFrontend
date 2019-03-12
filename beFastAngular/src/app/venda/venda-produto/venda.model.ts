91b48db362a422b9472f0c47ecd53c43472a1f46
Andre <andrehoinacki2012@gmail.com>
2019-03-11 21:31:00 -0300

    Alterações tela de vendas

--- beFastAngular/src/app/venda/venda-produto/venda.model.ts
+++ beFastAngular/src/app/venda/venda-produto/venda.model.ts
@@ -3,7 +3,7 @@ import { FormaPagamento } from "../forma-pagamento/formapgto.model";
 
 export class Venda {
     public id : number;
-    public data : Date;        
+    public dataVenda : Date;        
     public cliente : Usuario;
     public funcionario : Usuario;    
     public itens : Array<any> = [];
